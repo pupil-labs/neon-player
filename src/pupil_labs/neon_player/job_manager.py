f771ad9b8fe5948c96743d5545a9dc4ccded66da
Nikolai Kapralov <4dvlup@gmail.com>
2026-06-16 13:22:08 +0200

    fix: force-save settings before bg job launch and quit (#51)

--- src/pupil_labs/neon_player/job_manager.py
+++ src/pupil_labs/neon_player/job_manager.py
@@ -173,7 +173,7 @@ class JobManager(QObject):
             logging.warning("Not starting background job in headless mode")
             return
 
-        neon_player.instance().save_settings()
+        neon_player.instance().save_settings(force=True)
 
         recording = neon_player.instance().recording
         if recording is None:
